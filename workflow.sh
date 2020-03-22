54304f7857143db327a1144a65237a7f6f7aa156
Alex M <ialex11@engineer.com>
2020-03-22 10:08:40 +0100

    Create workflow.sh

---
+++ workflow.sh
@@ -0,0 +1,10 @@
+if [ -f Tweak.x ]; then
+  echo "Compiling theos tweak".
+  echo "Performing checks."
+  if [-f Makefile]
+    make package
+    make do
+  fi
+else
+  echo "Dunno what is happening. Skipping this process..."
+fi
